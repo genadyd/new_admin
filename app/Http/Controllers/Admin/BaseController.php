5611df051cb95c6d2369472bee7834c8e55d018f
genady <jobgenady@gmail.com>
2020-10-02 16:40:21 +0300

    catigories list render by js

--- app/Http/Controllers/Admin/BaseController.php
+++ app/Http/Controllers/Admin/BaseController.php
@@ -15,8 +15,8 @@ abstract class BaseController extends Controller
     protected function getIp(){
         $str = shell_exec("/sbin/ifconfig") ;
         $pattern = "/addr:(192\.168\.\d{2,3}\.\d{2,3})/";
-
         preg_match($pattern,$str,$matches);
+        if(!isset($matches[1])) return '';
         if(str_contains(\request()->fullUrl(),$matches[1])) {
             return '/new_admin/public';
         }elseif (str_contains(\request()->fullUrl(),'localhost')){
