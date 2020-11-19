de89e0cc9765012a7cea8d72b4b196bf64420715
genady <jobgenady@gmail.com>
2020-11-19 02:21:51 +0200

    system refactoring

--- resources/js/admin/modules/categories_module/index.ts
+++ resources/js/admin/modules/categories_module/index.ts
@@ -1,11 +1,4 @@
 
-import ListController from "./ListController";
-import FormController from "./FormController";
-import ListControlsController from "./ListControlsController";
-import FormControllersInterface from "../../app/controllers/forms_controllers/FormControllersInterface";
-import ListControllerInterface from "../../app/controllers/list_controllers/ListControllerInterface";
-import ListControlsControllerInterface
-    from "../../app/controllers/list_controllers/list_controls_controller/ListControlsControllerInterface";
 import CategoriesController from "./CategoriesController";
 
 
@@ -13,14 +6,8 @@ import CategoriesController from "./CategoriesController";
 CKEDITOR.replace('ckeditor_text',{
     customConfig: '../ckeditor/custom_config.js'
 })
-const formController:any = new CategoriesController()
-// const listControlsController:ListControlsControllerInterface = new ListControlsController()
-//
-// const listController:ListControllerInterface = new ListController()
-// listController.getControlsController(listControlsController)
-// listControlsController.setListRender(listController.renderList)
-//
-// formController.getListController(listController)
+new CategoriesController()
+
 
 
 
